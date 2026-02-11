6b5af75edd9cb7d05cd0e77d47700e2fc4567692
BrianLowDeQuan <132840000+BrianLowDeQuan@users.noreply.github.com>
2026-02-11 20:03:49 +0800

    Update app.js

--- app.js
+++ app.js
@@ -23,21 +23,21 @@ gifs.forEach(gifSrc => {
 // changes for when NO button is clicked
 no.addEventListener("click", () => {
   if (count == 2) {
-    gif.src = "resources/cat-heart.gif";// gif credit: https://knowyourmeme.com/photos/2738959-mr-fresh-side-eye-cat
+    gif.src = "resources/rusure.gif";
     text.innerHTML = "小臭臭你不是认真的吧?🤨";
     yes.style.height = "65%";
     yes.style.width = "60%";
     no.style.width = "30%";
     count++;
   } else if (count == 3) {
-    gif.src = "resources/3shocked-1.gif";// gif credit: https://tenor.com/view/tkthao219-peach-goma-gif-25008901
+    gif.src = "resources/3shocked-1.gif";
     text.innerHTML = "你手滑了是吗臭宝宝?🥹";
     yes.style.height = "70%";
     yes.style.width = "70%";
     no.style.width = "20%";
     count++;
   } else if (count == 4) {
-    gif.src = "resources/4.crying.gif";// gif credit: https://tenor.com/view/cat-kitty-gif-25340141
+    gif.src = "resources/4.crying.gif";
     text.innerHTML = "我要哭了小臭臭😭";
     yes.style.height = "80%";
     yes.style.width = "80%";
@@ -45,7 +45,7 @@ no.addEventListener("click", () => {
     no.style.width = "10%";
     count++;
   } else if (count == 5) {
-    gif.src = "resources/5.crying.gif";// gif credit: https://tenor.com/view/cat-gif-10173437195524493032
+    gif.src = "resources/5.crying.gif";
     text.innerHTML = "臭宝宝！！！！！！🥺😘";
     yes.style.height = "90%";
     yes.style.width = "96%";
@@ -56,9 +56,9 @@ no.addEventListener("click", () => {
 // changes for when YES button is clicked
 yes.addEventListener("click", () => {
   vid.style.display = "block";
-  gif.src = "resources/idc.gif";// gif credit: https://tenor.com/view/peach-cat-kiss-animated-love-mwah-gif-25743978
+  gif.src = "resources/idc.gif";
   text.innerHTML = "就知道我的小臭臭喜欢我！😘";
-  yes.innerHTML = '<a href="">see ya at 13th Feb 4pm</a>';// inside the " " put your social media profile link
+  yes.innerHTML = '<a href="">see ya at 13th Feb 4pm</a>';
   yes.style.height = "90%";
   yes.style.width = "96%";
   no.style.display = "none";
@@ -66,6 +66,7 @@ yes.addEventListener("click", () => {
     vid.style.display = "none";
   }, 9000);
 });
+
 
 
 
